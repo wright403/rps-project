fcf6ea6be84c0e022f4ac4e8a5d61d96d9b3663d
Amanda <aclaas522@gmail.com>
2022-03-08 17:15:56 -0600

    added to human, still need to finish human and then work on game

--- human.py
+++ human.py
@@ -4,28 +4,19 @@ class Human(Player):
     def __init__(self, name):
         super().__init__(name)
         pass
-
-
-    
-    
-    
+  
     def choose_gesture(self):
         user_input = input(f'please choose one of the following {self.gestures}: ')
             if user_input == 'rock':
-                
+                return 'rock'
             if user_input == 'paper':
-            
+                return 'paper'
             if user_input == 'scissors':
-            
+                return 'scissors'
             if user_input == 'lizard':
-            
-            if user_input == 'spock':    
-
-
-
-        
-        self.player_1 chooses 
-        print(user_input)
-        pass
+                return 'lizard'
+            if user_input == 'spock': 
+                return 'spock'   
+            pass
         
 # "copy and paste like 19 times"
